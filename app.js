15a7926e7f2aa3f71bfa175629a953631900090b
Seth Bowman <brandonsethbowman@gmail.com>
2021-07-07 11:37:41 -0500

    Added animations to text and images

--- app.js
+++ app.js
@@ -10,3 +10,44 @@ fetch(
 .catch((err) => {
     console.log(err);
 });
+
+
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.AnimatedHeading');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.AnimatedHeading .letter',
+    translateX: [40,0],
+    translateZ: 0,
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 1200,
+    delay: (el, i) => 500 + 30 * i
+  }).add({
+    targets: '.AnimatedHeading .letter',
+    translateX: [0,-30],
+    opacity: [1,0],
+    easing: "easeInExpo",
+    duration: 1100,
+    delay: (el, i) => 100 + 30 * i
+  });
+
+
+  anime.timeline({loop: true})
+  .add({
+    targets: '.ParaAnime .word',
+    scale: [14,1],
+    opacity: [0,1],
+    easing: "easeOutCirc",
+    duration: 800,
+    delay: (el, i) => 800 * i
+  }).add({
+    targets: '.ParaAnime',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+
